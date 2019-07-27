621542580d838b1063dbca5393150aa4485d5dfb
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-07-27 15:53:05 -0300

    Add Nodemailer, Send email for canceled appointments

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '916f8701179365',
+    pass: 'ed4f66a4b605f2',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
