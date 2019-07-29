c99970e81fd749092b8e0d10e58d53212a706b43
Thyago Araujo <thyagopcb.araujo@gmail.com>
2019-07-29 11:13:57 -0300

    Fix .env

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ import * as Sentry from '@sentry/node';
 import 'express-async-errors';
 import routes from './routes';
 import sentryConfig from './config/sentry';
-
 import './database';
 
 class App {
@@ -38,8 +37,10 @@ class App {
     this.server.use(async (err, req, res, next) => {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
+
         return res.status(500).json(errors);
       }
+
       return res.status(500).json({ error: 'Internal server error' });
     });
   }
